b004c0725801e9983f75411daf0c3968d1b085af
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2024-08-07 18:45:15 +0530

    3rd

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// const express = require("express");
+// require("dotenv").config();
+// const path = require("path");
+
+// const app = express();
+
+// const emailRoute = require("./routes/emailRoute");
+
+// app.use(express.json());
+
+// app.use("/api/emails", emailRoute);
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
@@ -12,7 +29,12 @@ app.use("/api/emails", emailRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    console.log(`EMAIL_USER: ${process.env.EMAIL_USER}`);
+    console.log(`EMAIL_PASS: ${process.env.EMAIL_PASS}`);
+});
+
 
 
 
--- services/EmailService.js
+++ services/EmailService.js
@@ -1,3 +1,38 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         type: "login",
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+// class EmailService {
+//     static async sendOTP(emailContent) {
+//         try {
+//             const mailOptions = {
+//                 from: '"Portfolio" <nodeapp53@gmail.com>',
+//                 to: "pasindugunasekara882@gmail.com",
+//                 subject: "Contacted you from your Portfolio",
+//                 html: emailContent,
+//             };
+
+//             const info = await transporter.sendMail(mailOptions);
+//             //console.log(`OTP code sent to kaveen.dinethma@gmail.com: ${info.response}`);
+//         } catch (error) {
+//             console.error("Error Email code:", error);
+//         }
+//     }
+// }
+
+// module.exports = EmailService;
+
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -23,14 +58,17 @@ class EmailService {
             };
 
             const info = await transporter.sendMail(mailOptions);
-            //console.log(`OTP code sent to kaveen.dinethma@gmail.com: ${info.response}`);
+            console.log(`Email sent: ${info.response}`);
+            return info.response;
         } catch (error) {
-            console.error("Error Email code:", error);
+            console.error("Error sending email:", error);
+            throw new Error("Error sending email");
         }
     }
 }
 
 module.exports = EmailService;
+
 
 
 
