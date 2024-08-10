176614f4045f5bf6245107c9f260091bbea02bd9
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2024-08-10 21:20:54 +0530

    update

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// require("dotenv").config();
-// const path = require("path");
-
-// const app = express();
-
-// const emailRoute = require("./routes/emailRoute");
-
-// app.use(express.json());
-
-// app.use("/api/emails", emailRoute);
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
@@ -29,11 +12,12 @@ app.use("/api/emails", emailRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    console.log(`EMAIL_USER: ${process.env.EMAIL_USER}`);
-    console.log(`EMAIL_PASS: ${process.env.EMAIL_PASS}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
 
 
 
