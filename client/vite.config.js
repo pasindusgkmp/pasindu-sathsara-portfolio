f4ec1ef7ff6946c836b9e521e9dc283b24af32ce
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2024-08-10 22:02:34 +0530

    vite.config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,21 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     logLevel: "info",
+//     server: {
+//       proxy: {
+//         "/api": {
+//           target: "http://localhost:5000",
+//           changeOrigin: true,
+//           secure: false,
+//         },
+//       },
+//     },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -12,6 +30,11 @@ export default defineConfig({
           changeOrigin: true,
           secure: false,
         },
+      },
+    },
+    build: {
+      rollupOptions: {
+        external: ['react-type-animation'],
       },
     },
 });
